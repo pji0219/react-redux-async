adc501987f3f9272189ad40c5d2ae441b081afe6
pji0219 <pji219@naver.com>
2023-03-04 00:16:34 +0900

    리덕스 비동기

---
+++ src/store/cart.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {},
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
---
+++ src/store/ui.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isCartVisible: false,
+};
+
+const uiSlice = createSlice({
+  name: 'ui',
+  initialState,
+  reducers: {
+    toggle(state) {
+      state.isCartVisible = !state.isCartVisible;
+    },
+  },
+});
+
+export const uiActions = uiSlice.actions;
+
+export default uiSlice;
