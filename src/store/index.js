d734aec4e1d7037e25793164c9d204bd1f3d1081
pji0219 <pji219@naver.com>
2023-03-04 23:45:00 +0900

    리덕스 비동기

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartSlice from './cart';
 import uiSlice from './ui';
 
 const store = configureStore({
   reducer: {
     ui: uiSlice.reducer,
+    cart: cartSlice.reducer,
   },
 });
 
